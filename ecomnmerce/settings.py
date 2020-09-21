3862a938fb1dc43d6ca0caa6140f82034d9fe9f1
rhutik <rhutikbhoir2002@gmail.com>
2020-09-21 16:10:40 +0530

    updated settings

--- ecomnmerce/settings.py
+++ ecomnmerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_8+)m)j$at6pazj(g=)^ntl=4f3-x4z_%pf@uby&&c*z*g-4uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rbcomputers.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn ecomnmerce.wsgi --log-file -
