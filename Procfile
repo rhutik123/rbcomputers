1a2220c9fa3075acc4507deb8515933583cd6485
rhutik <rhutikbhoir2002@gmail.com>
2020-09-21 15:54:04 +0530

    new try

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn ecomnmerce.wsgi --log-file -
--- ecomnmerce/settings.py
+++ ecomnmerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_8+)m)j$at6pazj(g=)^ntl=4f3-x4z_%pf@uby&&c*z*g-4uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rbcomputers.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,10 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,8 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATIC_URL = '/static/'
 
